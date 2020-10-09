df29a5f37ffa5121f35a1620ecd7cc87e380c301
Felipe França <contatofelipefranca12@gmail.com>
2020-10-09 14:39:30 -0300

    Adiciona a conclusão do exercício Sinal

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroDigitado;
+            string textoDigitado;
+            bool numeroValido;
+
+            Console.Clear();
+            Console.WriteLine("--- Sinal\n");
+            
+            Console.Write("Digite um número: ");
+            textoDigitado = Console.ReadLine();
+
+            numeroValido = Double.TryParse(textoDigitado, out numeroDigitado);
+            
+            if (!numeroValido)
+            {
+                Console.WriteLine("Por favor digite somente número(s)");
+            }
+            else if (numeroDigitado == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (numeroDigitado > 0)
+            {
+                Console.WriteLine("Positivo");
+            }
+            else if(numeroDigitado < 0)
+            {
+                Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Sinal
-[console: Sinal] Leia um número inteiro e exiba “Negativo”, “Zero” ou “Positivo”, conforme o caso.
+*[console: Sinal] Leia um número inteiro e exiba “Negativo”, “Zero” ou “Positivo”, conforme o caso.*
+
+`Exemplo`
+
+```
+Digite um número: 35
+Positivo
+```
+
+`ou`
+
+```
+Digite um número: -10
+Negativo
+```
+
+`ou`
+
+```
+Digite um número: 0
+Zero
+```
